59498db88ba79f87daffbb4a965520c8a1c68d8b
Cristina Mastellaro <cristinamastellaro8@gmail.com>
2025-10-24 10:47:43 +0200

    handle exceptions

--- src/main/java/cristinamastellaro/BE_U2_S3_Test/controllers/LoginController.java
+++ src/main/java/cristinamastellaro/BE_U2_S3_Test/controllers/LoginController.java
@@ -5,13 +5,9 @@ import cristinamastellaro.BE_U2_S3_Test.payloads.UserPayload;
 import cristinamastellaro.BE_U2_S3_Test.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/login")
 public class LoginController {
     @Autowired
